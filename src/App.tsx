04be2739f5afd7bdc90cf5f298ab962bbbbd9c11
yanasekenta <ynsknt0505@gmail.com>
2023-07-15 15:38:42 +0900

    ソート画面を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Box, Button, Center, HStack, VStack } from "@chakra-ui/react"
+
 function App() {
+  const array = [10, 2, 8, 17, 6]
 
   return (
-    <>
-    </>
+    <Center mt={10}>
+      <VStack>
+        <HStack>
+          {array.map(num => (
+            <Box w='40px' h='40px' textAlign='center' lineHeight='40px' border='1px solid gray'>{num}</Box>
+          ))}
+        </HStack>
+        <Button colorScheme="teal" size='sm'>バブルソート</Button>
+        <Button colorScheme="teal" size='sm'>ソート</Button>
+      </VStack>
+    </Center>
   )
 }
 
